ff88edee702a531173c7c46349659ed548cb8142
Nikhil Benesch <nikhil.benesch@gmail.com>
2018-09-10 14:22:09 -0400

    support cockroachdb

--- todo/database.py
+++ todo/database.py
@@ -4,6 +4,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+import cockroachdb
+
 engine = create_engine(app.config["DATABASE"], convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                         autoflush=False,
